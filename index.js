6864062f5305b1dfe5323b12f122bb226cc11861
Hayat <fahayat@deloitte.ca>
2020-02-29 19:55:45 -0500

    Working app with synced buttons

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
+let app = express();
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('index'))
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('event', function(msg){
+    io.emit('event', msg)
+  });
+});
