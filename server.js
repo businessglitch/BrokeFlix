f4da280d48247f41bf7dda7fda0b9fc446bcd7eb
Fahad Hayat <fahadhayat@outlook.com>
2020-08-07 14:48:21 -0400

    fixed path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv');
 const colors = require('colors')
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
